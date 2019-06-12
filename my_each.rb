efa923c2a522c4878aa6c5b4131a8338abb8aca8
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-12 19:17:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -11,12 +11,3 @@ collection = [1, 2, 3, 4]
 my_each(collection) do |i|
   i
 end
-
-def yield_test
-  puts "You are in the method"
-  yield
-  puts "You are back in the method"
-  yield
-end
-
-yield_test {puts 2**2}

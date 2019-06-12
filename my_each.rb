cabd7a70adc3844ae0966946e2dbf4bee5ad7e34
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-12 19:07:35 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
 def my_each # put argument(s) here
   # code here
 end
+
+def yield_test
+  puts "You are in the method"
+  yield
+  puts "You are back in the method"
+  yield
+end
+
+test {puts 2**2}
